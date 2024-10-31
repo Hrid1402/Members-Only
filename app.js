97eddf73762c5ddc6a7a560d0aa67c72108e2b45
Hrid1402 <Hrid1402@gmail.com>
2024-10-31 11:44:48 -0500

    feat: login working

--- app.js
+++ app.js
@@ -19,7 +19,51 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+passport.use(
+  new LocalStrategy({
+    usernameField: 'usernameEmail',
+    passwordField: 'password'
+  },async (username, password, done) => {
+    try {
+      console.log("tried");
+      const usernameRow = await db.selectUsername(username);
+      let user = null;
+      if(!usernameRow){
+        user = await db.selectEmail(username);
+      }else{
+        user = usernameRow;
+      }
+      
+      if (!user) {
+        console.log("Incorrect username or email");
+        return done(null, false, { message: "Incorrect username or email" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        console.log("Incorrect password");
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.selectByID(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+
+app.get("/", (req, res) => res.render("index", { user: req.user }));
 app.get("/sign-up", (req, res) => res.render("sign-up"));
 app.get("/log-in", (req, res) => res.render("log-in"));
 
@@ -27,12 +71,33 @@ app.get("/log-in", (req, res) => res.render("log-in"));
 app.post("/sign-up", async (req, res, next) => {
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
         try {
-            await db.addUser(req.body.firstName, req.body.lastName, req.body.email, req.body.username, hashedPassword);
-            res.redirect("/");
+            const user = await db.addUser(req.body.firstName, req.body.lastName, req.body.email, req.body.username, hashedPassword);
+
+            req.login(user, (err) => {
+              if (err) {
+                  return next(err);
+              }
+              res.redirect("/");
+          });
+  
         } catch(err) {
           return next(err);
         }
       });
   });
+
+app.post("/log-in", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in"
+  })
+);
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.listen(PORT, () => console.log("http://localhost:" + PORT + "/"));
--- db/queries.js
+++ db/queries.js
@@ -2,8 +2,27 @@ const pool = require("./pool");
 
 async function addUser(fn, sn, email, username, password){
     console.log(fn, sn, email, username, password);
-    await pool.query("INSERT INTO users (firstname, lastname, email, username, password) VALUES ($1, $2, $3, $4, $5)", [fn, sn, email, username, password]);
+    const {rows} = await pool.query("INSERT INTO users (firstname, lastname, email, username, password) VALUES ($1, $2, $3, $4, $5) RETURNING *", [fn, sn, email, username, password]);
+    return rows[0]; 
 }
+async function selectUsername(username){
+    console.log("userNameOrEmail: " + username);
+    const {rows} = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+    return rows[0];
+}
+async function selectEmail(email){
+    const {rows} = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    return rows[0];
+}
+async function selectByID(id){
+    const {rows} = await pool.query("SELECT * FROM users WHERE id = $1", [id])
+    return rows[0];
+}
+
+
 module.exports = {
-    addUser
+    addUser,
+    selectUsername,
+    selectEmail,
+    selectByID
 };
