7157fd487758e93cd5898418d321c94912f41f3c
Hrid1402 <Hrid1402@gmail.com>
2024-11-01 13:59:31 -0500

    feat: logic working, validation left

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("node:path");
 const db = require("./db/queries");
 const bcrypt = require('bcryptjs');
 const assetsPath = path.join(__dirname, "public");
+require('dotenv').config();
 
 const PORT = 5000
 
@@ -63,9 +64,30 @@ passport.deserializeUser(async (id, done) => {
 });
 
 
-app.get("/", (req, res) => res.render("index", { user: req.user }));
+app.get("/", async (req, res) => {
+  try {
+    const secrets = await db.getAllSecrets();
+    res.render("index", { user: req.user, secrets:secrets });
+} catch (error) {
+    console.error('Error fetching secrets:', error);
+    res.status(500).send('Internal Server Error');
+}
+});
+
 app.get("/sign-up", (req, res) => res.render("sign-up"));
 app.get("/log-in", (req, res) => res.render("log-in"));
+app.get("/addSecret", (req, res) => (!req.user) ? res.redirect("/") : res.render("addSecret"));
+app.get("/secretCode", (req, res) => (!req.user) ? res.redirect("/") : res.render("secretCode"));
+app.get("/mySecrets", async (req, res) => {
+  if(!req.user){
+    return res.redirect("/");
+  }else{
+    const secrets = await db.getAllSecretsById(req.user.id);
+    res.render("mySecrets", {secrets: secrets})
+  }
+});
+
+
 
 
 app.post("/sign-up", async (req, res, next) => {
@@ -77,7 +99,7 @@ app.post("/sign-up", async (req, res, next) => {
               if (err) {
                   return next(err);
               }
-              res.redirect("/");
+              return res.redirect("/");
           });
   
         } catch(err) {
@@ -85,7 +107,6 @@ app.post("/sign-up", async (req, res, next) => {
         }
       });
   });
-
 app.post("/log-in", passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/log-in"
@@ -96,8 +117,56 @@ app.get("/log-out", (req, res, next) => {
     if (err) {
       return next(err);
     }
-    res.redirect("/");
+    return res.redirect("/");
   });
 });
+
+
+function getCurrentDateFormatted() {
+  const d = new Date();
+  return `${String(d.getDate()).padStart(2, '0')}-${String(d.getMonth() + 1).padStart(2, '0')}-${d.getFullYear()}`;
+}
+
+app.post("/addSecret", async (req, res)=>{
+  if(!req.user){
+    res.redirect("/");
+  }else{
+    await db.addSecret(req.user.username, req.body.title, req.body.content, getCurrentDateFormatted(), req.user.id);
+    res.redirect("/");
+  }
+});
+app.post("/delete/:id&:author_id", async (req, res)=>{
+  if(!req.user){
+    return res.redirect("/");
+  }else if(req.user.admin){
+    await db.deleteSecretById(req.params.id);
+    return res.redirect("/");
+  }
+  else{
+    if(req.params.author_id == req.user.id){
+      await db.deleteSecretById(req.params.id);
+      return res.redirect("/");
+    }else{
+      console.log("Error, no permissions");
+      return res.redirect("/");
+    }
+  }
+  });
+app.post("/secretCode", async (req, res)=>{
+  if(!req.user){
+    return res.redirect("/");
+  }else{
+    console.log(req.body);
+    if(req.body.code == process.env.secretCode){
+      console.log("Passed");
+      await db.giveAdmin(req.user.id);
+      return res.redirect("/");
+    }else{
+      console.log("Invalid");
+      
+    }
+  }
+});
+  
 
 app.listen(PORT, () => console.log("http://localhost:" + PORT + "/"));
--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 async function addUser(fn, sn, email, username, password){
     console.log(fn, sn, email, username, password);
-    const {rows} = await pool.query("INSERT INTO users (firstname, lastname, email, username, password) VALUES ($1, $2, $3, $4, $5) RETURNING *", [fn, sn, email, username, password]);
+    const {rows} = await pool.query("INSERT INTO users (firstname, lastname, email, username, password, admin) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", [fn, sn, email, username, password, false]);
     return rows[0]; 
 }
 async function selectUsername(username){
@@ -15,14 +15,35 @@ async function selectEmail(email){
     return rows[0];
 }
 async function selectByID(id){
-    const {rows} = await pool.query("SELECT * FROM users WHERE id = $1", [id])
+    const {rows} = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
     return rows[0];
 }
-
+async function getAllSecrets(){
+    const {rows} = await pool.query("SELECT * FROM secrets ORDER BY id DESC");
+    return rows;
+}
+async function getAllSecretsById(id){
+    const {rows} = await pool.query("SELECT * FROM secrets WHERE author_id = $1 ORDER BY id DESC", [id]);
+    return rows;
+}
+async function addSecret(author, title, content, date, author_id){
+    await pool.query("INSERT INTO secrets (author, title, content, date, author_id) VALUES ($1, $2, $3, $4, $5)", [author, title, content, date, author_id])
+}
+async function deleteSecretById(id){
+    await pool.query("DELETE FROM secrets WHERE id = $1", [id])
+}
+async function giveAdmin(userId){
+    await pool.query("UPDATE users SET admin = TRUE WHERE id = $1", [userId]);
+}
 
 module.exports = {
     addUser,
     selectUsername,
     selectEmail,
-    selectByID
+    selectByID,
+    getAllSecrets,
+    addSecret,
+    getAllSecretsById,
+    deleteSecretById,
+    giveAdmin
 };
